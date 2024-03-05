3addde1e72d3e397e558030c3f66edb562a8c7e9
gusgoes <66867730+gusgoes@users.noreply.github.com>
2024-03-05 20:10:45 +0000

    Delete week02 directory

--- week02/bank.py
+++
@@ -1,4 +0,0 @@
-cent1=int(input('Insert your balance in cents: '))
-cent2=int(input('Insert value in cents that would you like to add: '))
-euro=(cent1+cent2)/100
-print(f'{euro}€ Euro')
--- week02/helloWorld.py
+++
@@ -1,5 +0,0 @@
-# helloWorld.py
-# Just promot Hello World
-# Author: Gustavo Fernandes
-
-print("Hello World")
