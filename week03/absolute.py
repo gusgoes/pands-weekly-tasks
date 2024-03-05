4eb1ecb48e5fdfae6d30681351916944c0a866dc
gusgoes <66867730+gusgoes@users.noreply.github.com>
2024-03-05 19:47:49 +0000

    Delete week03 directory

--- week03/absolute.py
+++
@@ -1,4 +0,0 @@
-number=float(input('Enter a number: '))
-absoluteValue = abs(number)
-
-print('The absolute value of {} is {}'.format(number, absoluteValue))
--- week03/sub.py
+++
@@ -1,4 +0,0 @@
-x=int(input('Enter the first number: '))
-y=int(input('Enter the second number: '))
-result=x-y
-print('{} minus {} is {}'.format(x,y,result))
