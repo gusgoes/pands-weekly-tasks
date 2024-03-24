1593f20b930ef901f75210f4f50f28dd66c3d2ce
Gustavo Fernandes <G00425776@atu.ie>
2024-03-24 20:10:10 +0000

    week07

--- week07/howmanye.py
+++ week07/howmanye.py
@@ -1,13 +1,18 @@
-#Write a program that reads in a text file and 
-#outputs the number of e's it contains. 
-#Think about what is being asked here, 
-#document any assumptions you are making.
+import sys
 
+filename = "text.txt"
 
-with open('text.txt', 'r') as file:
-    
-    contents = file.read()
-    
-    count = contents.count('e')
-    
-    print(f"There are {count} occurrences of 'e' on that file.")
+def count_letter_e(filename):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            count = content.count('e')
+            return count
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except:
+        print("An error occurred while reading the file.")
+
+count = count_letter_e(filename)
+if count is not None:
+    print(f"The file '{filename}' contains {count} occurrences of the letter 'e'.")
