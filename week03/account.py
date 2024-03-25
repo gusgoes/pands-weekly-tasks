31ee8c2704f98424e43cd9826c16fe24b33acc98
Gustavo Fernandes <G00425776@atu.ie>
2024-03-25 20:51:12 +0000

    week03 - Account

---
+++ week03/account.py
@@ -0,0 +1,3 @@
+accountNumber=((input('Please enter an 10 digit account number: ')))
+
+print('XXXXXX'+accountNumber[6]+accountNumber[7]+accountNumber[8]+accountNumber[9])
--- week03/accounts.py
+++
@@ -1,3 +0,0 @@
-accountNumber=((input('Please enter an 10 digit account number: ')))
-
-print('XXXXXX'+accountNumber[6]+accountNumber[7]+accountNumber[8]+accountNumber[9])
